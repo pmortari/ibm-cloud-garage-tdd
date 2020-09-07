1a04438e42fe211da403ffcde712f1579234bf27
Phillip Mortari <pmortari@gmail.com>
2020-09-07 14:30:54 -0300

    Pass the second test

--- server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
+++ server/01-fahrenheit-2-celsius/fahrenheit-2-celsius.spec.js
@@ -12,7 +12,10 @@ describe('a fahrenheitToCelsius function should', () => {
     expect(fahrenheitToCelsius(32)).toBe(0);
   });
 
-  it.todo('return 10 for 50');
+  it('return 10 for 50', () => {
+    expect(fahrenheitToCelsius(50)).toBe(10);
+  });
+
   it.todo('return 100 for 212');
   it.todo('return -40 for -40');
   it.todo('return -273.15 for -459.67');
