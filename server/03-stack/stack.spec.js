13af7b0b12d5ce8087a6c5d09600b8657ef1a8e1
Phillip Mortari <pmortari@gmail.com>
2020-09-14 22:20:09 -0300

    Stack pops same pushed element

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -1,18 +1,21 @@
 const stackFactory = () => {  
   let count = 0;
+  let element;
 
   return {
     isEmpty: () => count === 0,
     size: () => count,
-    push: () => {
+    push: (el) => {
       if(count === 2) throw new Error('capacity overflow error');
 
       count += 1;
+      element = el;
     },
     pop: () => {    
       if(count === 0) throw new Error('capacity underflow error');
 
       count -= 1;
+      return element;
     }
   }
 };
@@ -73,7 +76,10 @@ describe('a stack', () => {
     }).toThrowError('capacity underflow error');
   });
 
-  it.todo('pops the same one item when pushed');
+  it('pops the same one item when pushed', () => {
+    stack.push('a');
+    expect(stack.pop()).toBe('a');
+  });
   it.todo('pops two items with the most recent first');
   it.todo('accepts only a positive capacity');
 });
